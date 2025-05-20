a52f1e6ee572ea27ea83410b1734c21b95373590
danamdz <119626881+danamdz@users.noreply.github.com>
2025-05-19 22:09:25 -0600

    Update next.config.ts
    ignore any

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from "next";
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const nextConfig: NextConfig = {
   images: {
     domains: ["image.tmdb.org"],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
-  
+
 export default nextConfig;
